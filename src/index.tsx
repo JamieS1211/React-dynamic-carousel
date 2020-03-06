cc8899ba98d9bd225a901e48fe58a8e3cb315360
JamieS1211 <jamieshepherd1211@gmail.com>
2020-03-06 17:21:19 +0000

    Fixes, type improvements and config improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,33 @@
-import React, { useLayoutEffect, useReducer, useRef } from "react"
+import React, { useLayoutEffect, useReducer, useState } from "react"
 import { animated, useSpring } from "react-spring"
 
 import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft"
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight"
+
+interface IActionMovePrev {
+    type: "movePrev"
+    payload: {
+        onReachStart: IProps["onReachStart"]
+    }
+}
+
+interface IActionMoveNext {
+    type: "moveNext"
+    payload: {
+        elementCount: number
+        onReachEnd: IProps["onReachEnd"]
+    }
+}
+
+interface IActionSetGalleryWidth {
+    type: "setGalleryWidth"
+    payload: {
+        galleryWidth: number
+        elementWidth: IProps["elementWidth"]
+        fadeDistance: NonNullable<IProps["fadeDistance"]>
+        minPadding: NonNullable<IProps["minPadding"]>
+    }
+}
 
 const initialState = {
     galleryPosition: 0,
@@ -10,58 +35,33 @@ const initialState = {
     useableWidth: 0,
     elementsShown: 0,
     excessSpace: 0,
-    paddingComponentWidth: 1000
+    paddingComponentWidth: 20
 }
 
-export interface IState {
-    galleryPosition: number
-    galleryWidth: number
-    useableWidth: number
-    elementsShown: number
-    excessSpace: number
-    paddingComponentWidth: number
-}
+export type State = typeof initialState
 
-export interface IAction {
-    type: string
-    payload: {
-        elementCount?: number
-        onReachStart?: (arg0: IState) => void
-        onReachEnd?: (arg0: IState) => void
-        galleryWidth?: number
-        fadeDistance?: number
-        elementWidth?: number
-        minPadding?: number
-    }
-}
+export type Action = IActionMovePrev | IActionMoveNext | IActionSetGalleryWidth
 
-export interface IProps {
-    tiles: React.ReactNode[]
-    elementWidth: number
-    fadeDistance?: number
-    minPadding?: number
-    onReachEnd?: () => void
-    onReachStart?: () => void
-}
-
-function reducer(state: IState, action: IAction) {
+function reducer(state: State, action: Action) {
     switch (action.type) {
-        case "movePrev":
+        case "movePrev": {
             if (state.galleryPosition > 0) {
                 return { ...state, galleryPosition: state.galleryPosition - 1 }
             } else if (action.payload.onReachStart !== undefined) {
                 action.payload.onReachStart(state)
             }
             return state
-        case "moveNext":
+        }
+        case "moveNext": {
             if (state.galleryPosition < action.payload.elementCount - state.elementsShown) {
                 return { ...state, galleryPosition: state.galleryPosition + 1 }
             } else if (action.payload.onReachEnd !== undefined) {
                 action.payload.onReachEnd(state)
             }
             return state
-        case "setGalleryWidth":
-            const useableWidth = state.galleryWidth - 2 * action.payload.fadeDistance
+        }
+        case "setGalleryWidth": {
+            const useableWidth = action.payload.galleryWidth - 2 * action.payload.fadeDistance
             const elementsShown = Math.floor(useableWidth / (action.payload.elementWidth + action.payload.minPadding))
             const excessSpace = useableWidth - action.payload.elementWidth * elementsShown
             const totalComponentPadding = elementsShown < 2 ? 0 : excessSpace / (elementsShown - 1)
@@ -73,53 +73,65 @@ function reducer(state: IState, action: IAction) {
                 excessSpace,
                 paddingComponentWidth: totalComponentPadding
             }
+        }
         default:
             throw new Error("Invalid action type")
     }
 }
 
-const HorizontalGallery: React.FC<IProps> = (props) => {
-    const fadeDistance = props.fadeDistance === undefined ? 100 : props.fadeDistance
-    const elementWidth = props.elementWidth
-    const minPadding = props.minPadding === undefined ? 0 : props.minPadding
+export interface IProps {
+    tiles: React.ReactNode[]
+    elementWidth: number
+    fadeDistance?: number
+    minPadding?: number
+    onReachEnd?: (state?: State) => void
+    onReachStart?: (state?: State) => void
+}
 
-    const galleryTrack = useRef(null)
+const HorizontalGallery: React.FC<IProps> = (props) => {
+    const { tiles, elementWidth, onReachEnd, onReachStart } = props
+    const fadeDistance = props.fadeDistance || 100
+    const minPadding = props.minPadding || 20
+
+    const [galleryTrack] = useState(React.createRef<HTMLDivElement>())
 
     const [state, dispatch] = useReducer(reducer, initialState)
 
     useLayoutEffect(() => {
-        if (galleryTrack && galleryTrack.current) {
-            const updateSize = () =>
-                dispatch({
-                    type: "setGalleryWidth",
-                    payload: {
-                        galleryWidth: galleryTrack.current.getBoundingClientRect().width,
-                        fadeDistance,
-                        elementWidth,
-                        minPadding
-                    }
-                })
+        if (galleryTrack !== undefined) {
+            const element = galleryTrack.current
+            if (element !== null) {
+                const updateSize = () =>
+                    dispatch({
+                        type: "setGalleryWidth",
+                        payload: {
+                            galleryWidth: element.getBoundingClientRect().width,
+                            fadeDistance,
+                            elementWidth,
+                            minPadding
+                        }
+                    })
 
-            // This is an instantaneous delay, allowing the page to render a vertical scroll bar if needed,
-            // and then take that into account when settings size
-            setTimeout(() => updateSize(), 0)
+                // This is an instantaneous delay, allowing the page to render a vertical scroll bar if needed,
+                // and then take that into account when settings size
+                setTimeout(() => updateSize(), 1000)
 
-            window.addEventListener("resize", updateSize)
+                window.addEventListener("resize", updateSize)
 
-            return () => window.removeEventListener("resize", updateSize)
+                return () => window.removeEventListener("resize", updateSize)
+            }
         }
-        // Removing galleryTrack.current causes issues
-        // eslint-disable-next-line
-    }, [galleryTrack.current, fadeDistance, elementWidth, minPadding, dispatch])
+    }, [elementWidth, fadeDistance, galleryTrack, minPadding])
 
     const trackProps = useSpring({
         to: {
             transform: `translate(${fadeDistance - (elementWidth + state.paddingComponentWidth) * state.galleryPosition}px)`
         }
     })
+
     const paddingProps = useSpring({
         to: {
-            width: state.paddingComponentWidth
+            paddingRight: state.paddingComponentWidth
         }
     })
 
@@ -127,11 +139,18 @@ const HorizontalGallery: React.FC<IProps> = (props) => {
         <div style={{ display: "flex", position: "relative" }}>
             <div style={{ display: "flex", width: "100%", overflow: "hidden" }} ref={galleryTrack}>
                 <animated.div style={{ display: "flex", ...trackProps }}>
-                    {props.tiles.map((tile: Node, index: number) => (
-                        <React.Fragment key={index}>
-                            {index > 0 && <animated.div style={paddingProps} />}
+                    {tiles.map((tile, index) => (
+                        <animated.div
+                            key={index}
+                            style={{
+                                ...paddingProps,
+                                width: elementWidth,
+                                overflow: "hidden",
+                                visibility: index > state.galleryPosition - 2 && index < state.galleryPosition + state.elementsShown + 1 ? "visible" : "hidden"
+                            }}
+                        >
                             {tile}
-                        </React.Fragment>
+                        </animated.div>
                     ))}
                 </animated.div>
             </div>
@@ -145,12 +164,12 @@ const HorizontalGallery: React.FC<IProps> = (props) => {
                     left: "0",
                     height: "100%",
                     width: fadeDistance,
-                    background: `linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0))`
+                    background: "linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0))"
                 }}
             >
                 <KeyboardArrowLeft
-                    style={{ transform: `translate(-110%)`, opacity: state.galleryPosition > 0 ? 1 : 0.2 }}
-                    onClick={() => dispatch({ type: "movePrev", payload: { onReachStart: props.onReachStart } })}
+                    style={{ transform: "translate(-110%)", opacity: state.galleryPosition > 0 ? 1 : 0.2 }}
+                    onClick={() => dispatch({ type: "movePrev", payload: { onReachStart } })}
                 />
             </div>
 
@@ -163,15 +182,15 @@ const HorizontalGallery: React.FC<IProps> = (props) => {
                     right: "0",
                     height: "100%",
                     width: fadeDistance,
-                    background: `linear-gradient(to left, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0))`
+                    background: "linear-gradient(to left, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0))"
                 }}
             >
                 <KeyboardArrowRight
-                    style={{ transform: `translate(110%)`, opacity: state.galleryPosition < props.tiles.length - state.elementsShown ? 1 : 0.2 }}
+                    style={{ transform: "translate(110%)", opacity: state.galleryPosition < tiles.length - state.elementsShown ? 1 : 0.2 }}
                     onClick={() =>
                         dispatch({
                             type: "moveNext",
-                            payload: { elementCount: props.tiles.length, onReachEnd: props.onReachEnd }
+                            payload: { elementCount: tiles.length, onReachEnd }
                         })
                     }
                 />
